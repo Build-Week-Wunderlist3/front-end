f8152d82b4993821af0d1ed301a9baf4876b0de5
angelynaliem <angelynaliem@gmail.com>
2020-06-22 23:30:57 -0400

    Hamburger menu functioning for mobile/tablet view

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const menuButton = document.getElementById("menu-button");
 const x = document.getElementById("x");
-const nav = document.getElementById("nav");
+const menuOverLay = document.getElementById("menu-overlay");
+
 let click = 0;
+
 function menuToggle() {
-    if (click === 0) {
-        nav.style.display = "none";
+    if(click === 1) {
+        menuOverLay.style.display = "none";
         menuButton.style.display = "flex";
         x.style.display = "none";
-        click = 1;
+        click = 0;
     } else {
-        nav.style.display = "flex";
+        menuOverLay.style.display = "flex";
         menuButton.style.display = "none";
         x.style.display = "flex";
+        click = 1;
     }
-        click = 0;
 }
-menuButton.addEventListener("click", function () {
+
+menuButton.addEventListener("click", function() {
     menuToggle();
 });
-x.addEventListener("click", function () {
+
+x.addEventListener("click", function() {
     menuToggle();
 });
